d428d78aaf5be8fbdd7d99419ae45359d2dec531
im-aliafzal <87354624+im-aliafzal@users.noreply.github.com>
2022-08-05 10:41:09 +0500

    Changes

--- ProjectName/src/Redux/index.js
+++ ProjectName/src/Redux/index.js
@@ -3,5 +3,4 @@ import {combineReducers} from 'redux';
 const reducers = combineReducers({
   auth: () => null,
 });
-export const reducersArray = ['colorSelection', 'images', 'effects', 'addText'];
 export default reducers;
